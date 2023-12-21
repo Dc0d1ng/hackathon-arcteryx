c79dbaf37e478cd411fe801852b44822b20e8fd6
Isaac Ho <Isaachwho@gmail.com>
2023-12-20 20:55:42 -0800

    add onClick handler for fitsize

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import background from "../../assets/images/background.png";
 import background2 from "../../assets/images/background2.png";
-import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const landing = () => {
+  const handleClick = () => {
+    console.log("you've clicked me!");
+  };
   return (
     <>
-      <img src={background} />
-      <img src={background2} />
+      <img src={background} alt="top of arcteryx site" onClick={handleClick} />
+      <img src={background2} alt="bottom of arcteryx site" />
     </>
   );
 };
